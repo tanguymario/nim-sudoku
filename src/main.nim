92a3af37efc060b9f0fb4d3499de7b4a4186bc8d
Tanguy Cesaratto <tanguymario@users.noreply.github.com>
2020-11-24 11:54:01 +0900

    refactored

--- src/main.nim
+++ src/main.nim
@@ -1,8 +1,3 @@
-import algorithm
-import random
-
-randomize()
-
 type
    SudokuCell* = seq[int]
    SudokuGrid* = array[9, array[9, SudokuCell]]
@@ -13,6 +8,13 @@ proc full(g: SudokuGrid): bool =
    for row in g:
       for cell in row:
          if not cell.filled:
+            return false
+   return true
+
+proc `==`*(g1, g2: SudokuGrid): bool =
+   for row in 0 ..< 9:
+      for col in 0 ..< 9:
+         if g1[row][col] != g2[row][col]:
             return false
    return true
 
@@ -56,58 +58,51 @@ proc debug(g: SudokuGrid): string =
       for col in 0 ..< 9:
          result &= "(" & $row & ", " & $col & ") " & $g[row][col] & "\n"
 
-proc removePossibility(c: var SudokuCell, val: int): bool =
-   if not c.filled:
-      let idx = c.find(val)
+proc onFilled(g: var SudokuGrid, row, col: int)
+proc removePossibility(g: var SudokuGrid, row, col, val: int) =
+   if not g[row][col].filled:
+      let idx = g[row][col].find(val)
       if idx >= 0:
-         c.delete(idx)
-         return true
-   return false
+         g[row][col].delete(idx)
+         if g[row][col].filled:
+            g.onFilled(row, col)
+
+proc onFilled(g: var SudokuGrid, row, col: int) =
+   if g[row][col].filled:
+      for tmpRow in 0 ..< 9:
+         if tmpRow != row:
+            g.removePossibility(tmpRow, col, g[row][col][0])
+      for tmpCol in 0 ..< 9:
+         if tmpCol != col:
+            g.removePossibility(row, tmpCol, g[row][col][0])
+      for y in 0 ..< 3:
+         for x in 0 ..< 3:
+            let tmpRow = int(row / 3) * 3 + y
+            let tmpCol = int(col / 3) * 3 + x
+            if tmpRow != row and tmpCol != col:
+               g.removePossibility(tmpRow, tmpCol, g[row][col][0])
 
 proc createSudokuGrid*(m: array[9, array[9, int]]): SudokuGrid =
    for row in 0 ..< 9:
       for col in 0 ..< 9:
-         if m[row][col] != 0:
-            result[row][col].add(m[row][col])
+         if m[row][col] == 0:
+            result[row][col] = @[1, 2, 3, 4, 5, 6, 7, 8, 9]
          else:
-            for i in 1 .. 9:
-               result[row][col].add(i)
-
-proc checkBasic(g: var SudokuGrid) =
-   var canSolve = true
-   while canSolve:
-      canSolve = false
-      for row in 0 ..< 9:
-         for col in 0 ..< 9:
-            if not g[row][col].filled:
-               for tmpCol in 0 ..< 9:
-                  if g[row][tmpCol].filled:
-                     canSolve = canSolve or g[row][col].removePossibility(g[row][tmpCol][0])
-               for tmpRow in 0 ..< 9:
-                  if g[tmpRow][col].filled:
-                     canSolve = canSolve or g[row][col].removePossibility(g[tmpRow][col][0])
-               for y in 0 ..< 3:
-                  for x in 0 ..< 3:
-                     let tmpRow = int(row / 3) * 3 + y
-                     let tmpCol = int(col / 3) * 3 + x
-                     if g[tmpRow][tmpCol].filled:
-                        canSolve = canSolve or g[row][col].removePossibility(g[tmpRow][tmpCol][0])
+            result[row][col] = @[m[row][col]]
 
 proc solve(g: var SudokuGrid) =
-   g.checkBasic()
+   for row in 0 ..< 9:
+      for col in 0 ..< 9:
+         if g[row][col].filled:
+            g.onFilled(row, col)
 
-   var maxIter = 10
-   var iter = 0
-   while not g.full:
+   if not g.full:
+      echo "hmmm"
 
-      iter += 1
-      if iter >= maxIter:
-         break
-   echo iter
    echo (debug(g))
 
 proc main*() =
-   # var grid: SudokuGrid = [
+   # var grid = createSudokuGrid([
    #    [0, 0, 0, 0, 0, 0, 0, 0, 0],
    #    [0, 0, 0, 0, 0, 0, 0, 0, 0],
    #    [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -117,33 +112,57 @@ proc main*() =
    #    [0, 0, 0, 0, 0, 0, 0, 0, 0],
    #    [0, 0, 0, 0, 0, 0, 0, 0, 0],
    #    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-   # ]
+   # ])
 
    # Easy - Arizona Daily Wildcat: Tuesday, Jan 17th 2006
-   # var grid: SudokuGrid = [
-   #    [0, 0, 0, 2, 6, 0, 7, 0, 1],
-   #    [6, 8, 0, 0, 7, 0, 0, 9, 0],
-   #    [1, 9, 0, 0, 0, 4, 5, 0, 0],
-   #    [8, 2, 0, 1, 0, 0, 0, 4, 0],
-   #    [0, 0, 4, 6, 0, 2, 9, 0, 0],
-   #    [0, 5, 0, 0, 0, 3, 0, 2, 8],
-   #    [0, 0, 9, 3, 0, 0, 0, 7, 4],
-   #    [0, 4, 0, 0, 5, 0, 0, 3, 6],
-   #    [7, 0, 3, 0, 1, 8, 0, 0, 0],
-   # ]
+   var fullGrid = createSudokuGrid([
+      [4, 3, 5, 2, 6, 9, 7, 8, 1],
+      [6, 8, 2, 5, 7, 1, 4, 9, 3],
+      [1, 9, 7, 8, 3, 4, 5, 6, 2],
+      [8, 2, 6, 1, 9, 5, 3, 4, 7],
+      [3, 7, 4, 6, 8, 2, 9, 1, 5],
+      [9, 5, 1, 7, 4, 3, 6, 2, 8],
+      [5, 1, 9, 3, 2, 6, 8, 7, 4],
+      [2, 4, 8, 9, 5, 7, 1, 3, 6],
+      [7, 6, 3, 4, 1, 8, 2, 5, 9],
+   ])
+
+   var grid = createSudokuGrid([
+      [0, 0, 0, 2, 6, 0, 7, 0, 1],
+      [6, 8, 0, 0, 7, 0, 0, 9, 0],
+      [1, 9, 0, 0, 0, 4, 5, 0, 0],
+      [8, 2, 0, 1, 0, 0, 0, 4, 0],
+      [0, 0, 4, 6, 0, 2, 9, 0, 0],
+      [0, 5, 0, 0, 0, 3, 0, 2, 8],
+      [0, 0, 9, 3, 0, 0, 0, 7, 4],
+      [0, 4, 0, 0, 5, 0, 0, 3, 6],
+      [7, 0, 3, 0, 1, 8, 0, 0, 0],
+   ])
 
    # Easy - Arizona Daily Wildcat: Wednesday, Jan 18th 2006
-   var grid = createSudokuGrid([
-      [1, 0, 0, 4, 8, 9, 0, 0, 6],
-      [7, 3, 0, 0, 0, 0, 0, 4, 0],
-      [0, 0, 0, 0, 0, 1, 2, 9, 5],
-      [0, 0, 7, 1, 2, 0, 6, 0, 0],
-      [5, 0, 0, 7, 0, 3, 0, 0, 8],
-      [0, 0, 6, 0, 9, 5, 7, 0, 0],
-      [9, 1, 4, 6, 0, 0, 0, 0, 0],
-      [0, 2, 0, 0, 0, 0, 0, 3, 7],
-      [8, 0, 0, 5, 1, 2, 0, 0, 4],
-   ])
+   # var fullGrid = createSudokuGrid([
+   #    [1, 5, 2, 4, 8, 9, 3, 7, 6],
+   #    [7, 3, 9, 2, 5, 6, 8, 4, 1],
+   #    [4, 6, 8, 3, 7, 1, 2, 9, 5],
+   #    [3, 8, 7, 1, 2, 4, 6, 5, 9],
+   #    [5, 9, 1, 7, 6, 3, 4, 2, 8],
+   #    [2, 4, 6, 8, 9, 5, 7, 1, 3],
+   #    [9, 1, 4, 6, 3, 7, 5, 8, 2],
+   #    [6, 2, 5, 9, 4, 8, 1, 3, 7],
+   #    [8, 7, 3, 5, 1, 2, 9, 6, 4],
+   # ])
+
+   # var grid = createSudokuGrid([
+   #    [1, 0, 0, 4, 8, 9, 0, 0, 6],
+   #    [7, 3, 0, 0, 0, 0, 0, 4, 0],
+   #    [0, 0, 0, 0, 0, 1, 2, 9, 5],
+   #    [0, 0, 7, 1, 2, 0, 6, 0, 0],
+   #    [5, 0, 0, 7, 0, 3, 0, 0, 8],
+   #    [0, 0, 6, 0, 9, 5, 7, 0, 0],
+   #    [9, 1, 4, 6, 0, 0, 0, 0, 0],
+   #    [0, 2, 0, 0, 0, 0, 0, 3, 7],
+   #    [8, 0, 0, 5, 1, 2, 0, 0, 4],
+   # ])
 
    # Intermediate - Daily Telegraph January 19th "Diabolical"
    # var grid = createSudokuGrid([
@@ -159,7 +178,7 @@ proc main*() =
    # ])
 
    # Hard
-   # var grid: SudokuGrid = [
+   # var grid = createSudokuGrid([
    #    [0, 0, 0, 6, 0, 0, 4, 0, 0],
    #    [7, 0, 0, 0, 0, 3, 6, 0, 0],
    #    [0, 0, 0, 0, 9, 1, 0, 8, 0],
@@ -169,7 +188,7 @@ proc main*() =
    #    [0, 4, 0, 2, 0, 0, 0, 6, 0],
    #    [9, 0, 3, 0, 0, 0, 0, 0, 0],
    #    [0, 2, 0, 0, 0, 0, 1, 0, 0],
-   # ]
+   # ])
 
    # Test
    # var grid = createSudokuGrid([
@@ -189,5 +208,7 @@ proc main*() =
    grid.solve()
 
    echo(grid)
+
+   echo (if grid == fullGrid: "Correct!" else: "Incorrect")
 
 main()
